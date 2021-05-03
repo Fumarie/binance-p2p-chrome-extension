333e91727c4f265e4d655200787a01577ff36547
Fumarie <vla7412@mail.ru>
2021-05-03 17:02:59 +0300

    almost done

---
+++ binanceExt.js
@@ -0,0 +1,126 @@
+const div = document.querySelector('.Table__TableWarp-sc-1fy6eca-0 > table > tbody').childNodes
+
+window.setTimeout(newIds, 5000)
+
+function newIds() {
+    document.body.style.border = "5px solid red";
+
+    chrome.storage.local.get('orders', function (orders) {
+        let backendIds = []
+
+        let ids = []
+
+        console.log(orders.orders)
+        if(orders.orders) {
+            console.log('orders', orders.orders)
+
+            backendIds = Object.keys(orders.orders)
+            console.log(Object.keys(orders.orders))
+        }
+        console.log(backendIds)
+        div.forEach((elem, i) => {
+            if(i % 2 === 0) {
+                const id = elem.firstChild.firstChild.childNodes[2].innerText.toString()
+                if (!backendIds.includes(id)) {
+                    console.log('WE FIND ID!!!!', id)
+                    ids.push(id)
+                }
+            }
+        })
+        console.log('newIds: ', ids)
+        addButtons(ids)
+    })
+    // addButtons(['20199773188304527360', '20191773042761793536'])
+}
+
+function addButtons(ids) {
+    div.forEach((elem, i) => {
+        if(i % 2 === 0) {
+
+            ///
+            const statusData = elem.nextSibling.childNodes[4]
+            if(statusData) {
+                const orderStatus = statusData.firstChild.firstChild.innerText
+                if(orderStatus !== 'Завершено') return
+                console.log(orderStatus)
+            }
+            ///
+
+            const id = elem.firstChild.firstChild.childNodes[2].innerText.toString()
+            console.log('ids', ids)
+            console.log(id)
+            if(!ids.includes(id)) return
+
+
+            const td = elem.nextSibling.childNodes[5]
+            console.log('td', td)
+
+            const syncButton = document.createElement('button')
+            syncButton.innerText = "٩(͡๏̯͡๏)۶"
+            syncButton.style.margin = "0px 0px 45px 0px"
+            syncButton.onclick = onButtonClick;
+
+            console.log(syncButton)
+
+            td.appendChild(syncButton)
+        }
+    })
+}
+
+function onButtonClick(event) {
+    const row = event.target.parentNode.parentNode
+
+    const sum = row.childNodes[1].firstChild.firstChild.innerText
+    const rate = row.childNodes[2].firstChild.firstChild.childNodes[1].firstChild.innerText
+    const currency = row.childNodes[2].firstChild.firstChild.childNodes[1].childNodes[1].innerText
+    const volume = row.childNodes[2].firstChild.childNodes[1].childNodes[1].firstChild.innerText
+
+    console.log('sum', sum)
+    console.log('rate', rate)
+    console.log(currency)
+    console.log(volume)
+
+    const idRow = row.previousSibling.firstChild.firstChild
+
+    const type = idRow.childNodes[0].innerText
+    const id = idRow.childNodes[2].innerText
+    const date = idRow.childNodes[3].innerText
+
+    console.log(type, id, date)
+
+    const data = {
+        sum,
+        rate,
+        currency,
+        volume,
+        type,
+        id,
+        date
+    }
+    console.log('-------------------------------------------------------------------')
+    try {
+        chrome.storage.local.get('orders', function (value) {
+            let object = {}
+            if(value) {
+                console.log('get: ', value)
+                object = {...value.orders, [data.id]: {...data}}
+
+                console.log('object', object)
+                chrome.storage.local.set({orders: {...object}}, function () {
+                        console.log('Saved: ', data)
+                    }
+                )
+            } else {
+                chrome.storage.local.set({orders: {[data.id]: {...data}}}, function () {
+                        console.log('Saved: ', data)
+                    }
+                )
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+
---
+++ popup.js
@@ -0,0 +1,121 @@
+// document.addEventListener('DOMContentLoaded', function(){
+//     const div = document.createElement("div")
+//     div.className = "orders"
+//     const sumP = document.createElement("p")
+//     sumP.innerHTML = 0
+//     div.innerHTML = sumP
+//     document.body.appendChild(div)
+//     console.log(div)
+//
+//     // chrome.storage.local.clear()
+//     chrome.storage.local.get('orders', function (value) {
+//         if(!value || Object.keys(value).length === 0) return
+//         console.log(value)
+//
+//         const data = {
+//             sum: 0
+//         }
+//
+//         Object.values(value.orders).forEach(element => {
+//
+//             //Считаем сумму всех сумм :)
+//             const sum = element.sum.split(',').join('')
+//             console.log(sum)
+//             data.sum += Number(sum)
+//         })
+//
+//         console.log(data.sum)
+//         sumP.innerText = data.sum
+//     })
+// })
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.local.get('orders', function (value) {
+        if (!value || Object.keys(value).length === 0) return;
+        console.log(value);
+
+        let crypto_buy = {
+            sum: 0,
+            volume: 0,
+            count: 0
+        };
+
+        let crypto_sell = {
+            sum: 0,
+            volume: 0,
+            count: 0
+        };
+
+        Object.values(value.orders).forEach((element) => {
+            const sum = Number(element.sum.split(",").join(""));
+            const volume = Number(element.volume.split(",").join(""));
+
+            //USDT!
+            if (element.currency === "/USDT") {
+                if (element.type === "Продать") {
+                    crypto_sell.sum += sum;
+                    crypto_sell.count++;
+                    crypto_sell.volume += volume;
+                } else {
+                    crypto_buy.sum += sum;
+                    crypto_buy.count++;
+                    crypto_buy.volume += volume;
+                }
+            }
+        });
+
+        let buy_rate = 0;
+        if (crypto_buy.sum / crypto_buy.volume) {
+            buy_rate = (crypto_buy.sum / crypto_buy.volume).toFixed(2);
+        }
+
+        let sell_rate = 0;
+        if (crypto_sell.sum / crypto_sell.volume) {
+            sell_rate = (crypto_sell.sum / crypto_sell.volume).toFixed(2);
+        }
+
+        let expected_profit = 0;
+        let expected_profit_percent = 0;
+
+        if (crypto_buy.volume && sell_rate && buy_rate) {
+            expected_profit =
+                crypto_buy.volume * sell_rate - crypto_buy.volume * buy_rate;
+            expected_profit_percent =
+                (expected_profit / (crypto_buy.volume * sell_rate)) * 100;
+        }
+
+        let profit = 0;
+        if (crypto_buy.sum && crypto_sell.sum) {
+            profit = crypto_sell.sum - crypto_buy.sum;
+        }
+
+        document.body.insertAdjacentHTML(
+            "beforeend",
+            `
+    <div>Общая сумма покупок USDT: 
+      ${crypto_buy.sum}
+      (${crypto_buy.count} deals)
+    </div>
+    <div>
+    Средний курс:
+    ${buy_rate}
+    </div>
+    <hr />
+    <div>Общая сумма продаж USDT: 
+      ${crypto_sell.sum}
+      (${crypto_sell.count} deals)
+    </div>
+    <div>
+      Средний курс:
+      ${sell_rate}
+    </div>
+    <hr />
+    <div>
+    Ожидаемый профит ${expected_profit}(${expected_profit_percent}%)
+    <div>
+    </div>
+    Действительный профит ${profit}
+    </div>
+    `
+        );
+    })
+})
